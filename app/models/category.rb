16893872807c5aac308b9314bf703238a738c970
ntxtthomas <apm.tthomas@gmail.com>
2026-05-08 16:49:03 -0500

    rubocop

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,6 @@
 class Category < ApplicationRecord
-	has_many :flashcards, dependent: :destroy
+  has_many :flashcards, dependent: :destroy
 
-	validates :name, presence: true
-	validates :slug, presence: true, uniqueness: true
+  validates :name, presence: true
+  validates :slug, presence: true, uniqueness: true
 end
